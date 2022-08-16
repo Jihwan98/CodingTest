df3f7c87f097f331671e36acb58851deb797b28d
Jihwan98 <76936390+Jihwan98@users.noreply.github.com>
2022-08-16 18:00:32 +0900

    Add files via upload

---
+++ ch3_greedy/8-16/2_큰 수의 법칙.py
@@ -0,0 +1,12 @@
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort(reverse=True)
+result, count = 0, 0
+for i in range(m):
+    if count >= k:
+        result += data[1]
+        count = 0
+    else:
+        result += data[0]
+        count += 1
+print(result)
